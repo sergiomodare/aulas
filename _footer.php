dc3a086eff7a5d79a14d474248609c813a710a33
sergiomodare <59795267+sergiomodare@users.noreply.github.com>
2020-01-26 22:00:24 -0300

    treino de casa

---
+++ _footer.php
@@ -0,0 +1,19 @@
+</main>
+<footer class="footer">
+  <a href="index.php" title="Ir para a página inicial"><i class="fas fa-fw fa-home"></i></a>
+  <div>
+    &copy; Copyright 2020 Sérgio Rock
+    <div>
+      <a href="contatos.php">&nbsp;Faça contato&nbsp;</a>
+      |
+      <a href="privacidade.php">&nbsp;Privacidade&nbsp;</a>
+    </div>
+  </div>
+  <a href="#topo" title="Ir para o topo da página"><i class="fas fa-fw fa-arrow-alt-circle-up"></i></a>
+</footer>
+</div>
+
+<script src="/senac/js/jquery.min.js"></script>
+</body>
+
+</html>
---
+++ _header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html> 
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+  <link rel="shortcut icon" href="/senac/img/logo01.jpg">
+  <link rel="stylesheet" href="/senac/css/global.css">
+  <link rel="stylesheet" href="/senac/css/all.min.css" />
+  <title>Ao próximo</title>
+  <style>
+
+  </style>
+</head>
+
+<body>
+  <a id="topo"></a>
+  <div class="wrap">
+
+    <header class="header">
+      <a href="index.php"><img src="https://picsum.photos/90/90" alt="Aopróximo" title="Home Page do Gatolândia"></a>
+      <h1>Ele sangrou por você<small>Faça algo ao próximo</small></h1>
+    </header>
+
+    <nav class="nav">
+      <a href="index.php"><i class="fas fa-fw fa-home"></i></a>
+      <a href="gatas"><i class="fas fa-female"></i></a>
+      <a href="sobre"><i class="fas fa-info"></i></a>
+    </nav>
+
+    <main class="main">
